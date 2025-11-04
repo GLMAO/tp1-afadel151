780b121b434215cbd52d7a5cef982edf1c5979c2
Fadel Akram <af8356207@gmail.com>
2025-11-04 12:47:53 +0100

    graphical user interface with swing

---
+++ tp-gl-master/gui/src/main/java/org/emp/gl/HorlogeGUI.java
@@ -0,0 +1,53 @@
+package org.emp.gl;
+
+import java.beans.PropertyChangeEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+import org.emp.gl.timer.service.TimerChangeListener;
+import org.emp.gl.timer.service.TimerService;
+
+public class HorlogeGUI implements TimerChangeListener {
+
+    String name;
+    JFrame frame = new JFrame();
+    JLabel label = new JLabel();
+    private final TimerService timerService;
+
+    public HorlogeGUI(String name, TimerService timerService) {
+        this.name = name;
+        this.timerService = timerService;
+        System.out.println("Horloge " + name + " initialized! with timer service");
+        init();
+    }
+
+    private void init() {
+        this.timerService.addTimeChangeListener(this);
+        frame.setTitle("Horloge: " + name);
+        frame.setSize(300, 100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(label);
+        frame.setVisible(true);
+        updateLabel(); 
+    }
+
+    public void updateLabel() {
+        if (timerService != null) {
+            String time = String.format("%02d:%02d:%02d",
+                    timerService.getHeures(),
+                    timerService.getMinutes(),
+                    timerService.getSecondes());
+            label.setText(time);
+        }
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent event) {
+        if (TimerChangeListener.SECONDE_PROP.equals(event.getPropertyName())) {
+            SwingUtilities.invokeLater(this::updateLabel);
+        }
+    }
+}
--- tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
+++ tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
@@ -3,9 +3,10 @@ package org.emp.gl.core.launcher;
 import java.util.Random;
 
 import org.emp.gl.clients.CompteARebours;
-import org.emp.gl.clients.Horloge ;
+import org.emp.gl.clients.Horloge;
 import org.emp.gl.time.service.impl.DummyTimeServiceImpl;
 import org.emp.gl.timer.service.TimerService;
+import org.emp.gl.HorlogeGUI;
 /**
  * Hello world!
  *
@@ -14,11 +15,10 @@ public class App {
 
     public static void main(String[] args) {
         TimerService Service = new DummyTimeServiceImpl();
-        testDuTimeServiceCompteur(Service); // remplace par testDuTimeServiceHorloge
+        testDuTimeServiceHorlogeGUI(Service); // remplace par testDuTimeServiceHorloge
     }
 
-    private static void testDuTimeServiceCompteur(TimerService Service)
-    {
+    private static void testDuTimeServiceCompteur(TimerService Service) {
         Random random = new Random();
         int min = 10;
         int max = 20;
@@ -30,13 +30,17 @@ public class App {
         ((DummyTimeServiceImpl) Service).setMinutes(4);
     }
 
-    private static void testDuTimeServiceHorloge(TimerService Service)
-    {
-        Horloge horloge1 = new Horloge("horloge 1",Service);
-        Horloge horloge2 = new Horloge("horloge 2",Service);
-        Horloge horloge3 = new Horloge("horloge 3",Service);
-        Horloge horloge4 = new Horloge("horloge 4",Service);
-        Horloge horloge5 = new Horloge("horloge 5",Service);
+    private static void testDuTimeServiceHorlogeGUI(TimerService Service) {
+        HorlogeGUI gui = new HorlogeGUI("horloge 1", Service);
+        Service.addTimeChangeListener(gui);
+    }
+
+    private static void testDuTimeServiceHorloge(TimerService Service) {
+        Horloge horloge1 = new Horloge("horloge 1", Service);
+        Horloge horloge2 = new Horloge("horloge 2", Service);
+        Horloge horloge3 = new Horloge("horloge 3", Service);
+        Horloge horloge4 = new Horloge("horloge 4", Service);
+        Horloge horloge5 = new Horloge("horloge 5", Service);
         Service.addTimeChangeListener(horloge1);
         Service.addTimeChangeListener(horloge2);
         Service.addTimeChangeListener(horloge3);
