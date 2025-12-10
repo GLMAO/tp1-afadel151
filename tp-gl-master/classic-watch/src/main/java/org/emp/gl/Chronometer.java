2271403a7dbcbd956ed4fd0e2136c093732db6be
afadel151 <af8356207@gmail.com>
2025-12-10 21:39:42 +0100

    updated chronometer

--- tp-gl-master/classic-watch/src/main/java/org/emp/gl/Chronometer.java
+++ tp-gl-master/classic-watch/src/main/java/org/emp/gl/Chronometer.java
@@ -6,41 +6,50 @@ import org.emp.Lookup;
 import org.emp.gl.timer.service.TimerChangeListener;
 import org.emp.gl.timer.service.TimerService;
 
-public class Chronometer implements TimerChangeListener{
- 
+public class Chronometer implements TimerChangeListener {
+
     private int dizieme = 0;
     private int secondes = 0;
 
     private boolean running = false;
 
-    public Chronometer()
-    {
+    public Chronometer() {
         TimerService ts = Lookup.getInstance().getService(TimerService.class);
         ts.addTimeChangeListener(this);
     }
-    public void start() { running = true; }
-    public void stop() { running = false; }
+
+    public void start() {
+        running = true;
+    }
+
+    public void stop() {
+        running = false;
+    }
+
     public void reset() {
         dizieme = 0;
         secondes = 0;
     }
-    public boolean isRunning() { return running; }
+
+    public boolean isRunning() {
+        return running;
+    }
     @Override
-    public void propertyChange(PropertyChangeEvent e) {
-        if (!running) return;
-
-        switch (e.getPropertyName()) {
-            case DIXEME_DE_SECONDE_PROP:
-                dizieme = (dizieme + 1) % 10;
-                if (dizieme == 0){
-                    secondes = (secondes + 1) % 60;
-                }
-                break;
-
-            default:
-                break;
+    public void propertyChange(PropertyChangeEvent evt) {
+        if (!running) return;             
+        dizieme++;
+        if (dizieme == 10) {
+            dizieme = 0;
+            secondes++;
         }
-    }    
-    public int getSecondes() { return secondes; }
-    public int getDizieme() { return dizieme; }
+    }
+    
+
+    public int getSecondes() {
+        return secondes;
+    }
+
+    public int getDizieme() {
+        return dizieme;
+    }
 }
